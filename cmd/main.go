a3a7f902d43a0dd2bdd2ee301572d06b380b8c74
Dmitry Sokolov <sokolov333d@gmail.com>
2025-11-27 16:59:24 +0300

    started app

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"adv-demo/configs"
+	"adv-demo/internal/auth"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	conf := configs.LoadConfig()
+	router := http.NewServeMux()
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: conf,
+	})
+
+	server := http.Server{
+		Addr: ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server on port 8081")
+	server.ListenAndServe()
+}
---
+++ internal/auth/auth.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+	"adv-demo/configs"
+	"adv-demo/pkg/req"
+	"adv-demo/pkg/res"
+	"fmt"
+	"net/http"
+)
+
+type AuthHandlerDeps struct {
+	*configs.Config
+}
+
+type AuthHandler struct {
+	*configs.Config
+}
+
+func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
+	handler := &AuthHandler{
+		Config: deps.Config,
+	}
+	router.HandleFunc("POST /auth/login", handler.Login())
+	router.HandleFunc("POST /auth/register", handler.Register())
+}
+
+func (handler *AuthHandler) Login() http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		body, err := req.HandleBody[LoginRequest](&w, r)
+		if err != nil {
+			return
+		}
+		fmt.Println(body)
+		data := LoginResponse{
+			Token: "12345",
+		}
+		res.Json(w, data, 200)
+	}
+}
+
+func (handler *AuthHandler) Register() http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+				body, err := req.HandleBody[RegisterRequest](&w, r)
+		if err != nil {
+			return
+		}
+		fmt.Println(body)
+	}
+}
