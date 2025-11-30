fba11b16371ce6286842c3f45785741e8ec7bba1
Dmitry Sokolov <sokolov333d@gmail.com>
2025-11-30 10:30:11 +0300

    added link handler

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adv-demo/configs"
 	"adv-demo/internal/auth"
+	"adv-demo/internal/link"
 	"adv-demo/pkg/db"
 	"fmt"
 	"net/http"
@@ -12,9 +13,12 @@ func main() {
 	conf := configs.LoadConfig()
 	_ = db.NewDb(conf)
 	router := http.NewServeMux()
+
+	// Handlers
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
 		Config: conf,
 	})
+	link.NewLinkHandler(router, link.LinkHandlerDeps{})
 
 	server := http.Server{
 		Addr: ":8081",
---
+++ internal/link/handler.go
@@ -0,0 +1,48 @@
+package link
+
+import (
+	"adv-demo/configs"
+	"fmt"
+	"net/http"
+)
+
+type LinkHandlerDeps struct {
+	*configs.Config
+}
+
+type LinkHandler struct {
+	*configs.Config
+}
+
+func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
+	handler := &LinkHandler{}
+	router.HandleFunc("POST /link", handler.Create())
+	router.HandleFunc("PATCH /link/{id}", handler.Update())
+	router.HandleFunc("DELETE /link/{id}", handler.Delete())
+	router.HandleFunc("GET /{hash}", handler.GoTo())
+}
+
+func (handler *LinkHandler) Create() http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (handler *LinkHandler) Update() http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (handler *LinkHandler) Delete() http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Println(id)
+	}
+}
+
+func (handler *LinkHandler) GoTo() http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+
+	}
+}
